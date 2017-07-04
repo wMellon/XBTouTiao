0b4bacec37c73793d76e2613b6763c4ef8ffcc12
ChenZhenChun <346891964@qq.com>
2017-07-04 09:20:44 +0800

    Initial Commit

---
+++ XBTouTiao/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XBTouTiao
+//
+//  Created by xxb on 2017/7/4.
+//  Copyright © 2017年 com.xb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XBTouTiao/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XBTouTiao
+//
+//  Created by xxb on 2017/7/4.
+//  Copyright © 2017年 com.xb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
